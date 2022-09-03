43d2a8d0aeb5facaaa94415f9af8f84b645aaa7d
haci pasayev <hacipasayev1@gmail.com>
2022-09-03 18:39:57 +0400

    evisi

---
+++ src/class_object/Book.java
@@ -0,0 +1,14 @@
+package class_object;
+
+public class Book {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int id;
+String name;
+String author;
+int pageCount;
+String description;
+	}
+
+}
---
+++ src/class_object/MainClass.java
@@ -0,0 +1,13 @@
+package class_object;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book kitab = new Book();
+		Book defter = new Book();
+		kitab.id = 12;
+		kitab.name = "Nizami";
+	}
+
+}
