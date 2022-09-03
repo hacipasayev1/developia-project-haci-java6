c5896a15d39b223623f2fb794eb243c22f89ca1e
haci pasayev <hacipasayev1@gmail.com>
2022-09-03 16:05:13 +0400

    sekil3

---
+++ src/Sekil3.java
@@ -0,0 +1,11 @@
+
+public class Sekil3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(((25.5*3.5-3.5*3.5)/(40.5-4.5)));
+		double a=25.5,b=3.5,c=40.5,d=4.5;
+		System.out.println(((a*b-b*b)/(c-d)));
+	}
+
+}
