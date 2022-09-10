250f6b742810da99443f935a198b4c38ce15bfeb
haci pasayev <hacipasayev1@gmail.com>
2022-09-10 16:07:33 +0400

    Cuteded
    kod islemedi

---
+++ src/constructors/home/Cuteded.java
@@ -0,0 +1,17 @@
+package constructors.home;
+
+public class Cuteded {
+public static void main(String[] args) {
+	int eded=20;
+			int b=0;
+			for(int i=1;i<=eded;i++) {
+	while(i!=0) {
+		b+=b%10;
+		b/=10;
+	}
+	if(b/2==0) {
+		System.out.println(eded);
+	}
+			}
+}
+}
