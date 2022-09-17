2555ef62b0bc42cea207b6f23b67240d0b690944
haci pasayev <hacipasayev1@gmail.com>
2022-09-17 15:39:58 +0400

    Update Metodlar.java

--- src/Ders7_Ev_isi/Metodlar.java
+++ src/Ders7_Ev_isi/Metodlar.java
@@ -2,17 +2,43 @@ package Ders7_Ev_isi;
 
 public class Metodlar {
 int m1(int begin,int end) {
-	int eded=0;
 	int ededi_orta=0;
 	int ededsayi=0;
-	while(eded!=0) {
+	int ededcemi=0;
+	if(begin>end){
+		
+		
+		
+		while(end!=begin) {
+	
+
+
+	while(end!=0) {
 		
 		ededsayi++;
-		int qaliq=eded%10;
-		int ededcemi=0;
+		int qaliq=end%10;
+		
 		 ededcemi+=qaliq;
-		 ededi_orta=ededcemi/ededsayi;
+		end/=10;
 	}
+	 ededi_orta=ededcemi/ededsayi;
 	System.out.println(ededi_orta);
+	end++;
+	}
+}else if(end>begin) {
+	while(end!=begin) {
+		while(end!=0) {
+			
+			ededsayi++;
+			int qaliq=end%10;
+			
+			 ededcemi+=qaliq;
+			end/=10;
+		}
+		 ededi_orta=ededcemi/ededsayi;
+		System.out.println(ededi_orta);
+		end++;
+	}
+}
 }
 }
