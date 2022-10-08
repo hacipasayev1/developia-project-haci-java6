50033f1216de40a1eed7d0b745ac8c0476dea45a
haci pasayev <hacipasayev1@gmail.com>
2022-10-08 18:50:40 +0400

    calisma3

---
+++ src1/calismalar/calisma3.java
@@ -0,0 +1,7 @@
+package calismalar;
+
+public class calisma3 {
+enum bizim yazdigimizdan ne artiq ne eksik obyekt yaratmaga icaze vermir
+enum limitli sayda obyekt yaratmaga icaze verir
+enum adi sinif ile eynidir
+}
