fc2f8eb39b3cbe3f4f4d55ea86323254725f2627
haci pasayev <hacipasayev1@gmail.com>
2022-10-08 18:42:05 +0400

    calisma1

---
+++ src1/calismalar/calismalar.java
@@ -0,0 +1,23 @@
+package calismalar;
+
+public class calismalar {
+	public class SomethingIsWrong {
+	    public static void main(String[] args) {
+	        Rectangle myRect;
+	        myRect.width = 40;
+	        myRect.height = 50;
+	        System.out.println("myRect's area is " + myRect.area());
+	    }
+	}
+
+	bu kodda hansı səfliklər var?
+	yazılı olaraq bir neçə cümlə ilə izah yaz
+	
+	
+	
+	Rectangle sinifi yaradilmiyib
+	bu sinifde int tipinde width height deyisenleri yaradilmayib
+		
+		
+		birde area metodu yaradilmiyib
+}
