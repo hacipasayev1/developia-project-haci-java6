513358599614494fd177a980955dde0625b29b31
haci pasayev <hacipasayev1@gmail.com>
2022-12-24 14:35:21 +0400

    student crud part 1

---
+++ student-crud/student-crud/src/test/java/az/developia/studentcrud/StudentCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package az.developia.studentcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
