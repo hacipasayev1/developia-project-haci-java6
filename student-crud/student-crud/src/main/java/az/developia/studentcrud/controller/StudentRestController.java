3deb5537411f435b587fc0c8fb6632968f3a4459
haci pasayev <hacipasayev1@gmail.com>
2023-01-04 15:09:18 +0400

    student-crud part 5 telebenin redaktesi

--- student-crud/student-crud/src/main/java/az/developia/studentcrud/controller/StudentRestController.java
+++ student-crud/student-crud/src/main/java/az/developia/studentcrud/controller/StudentRestController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,5 +30,15 @@ private StudentRepository studentRepository;
 	@GetMapping
 	public List<Student> findAll(){
 		return studentRepository.findAll();
+	}
+	
+	@DeleteMapping(path="/{id}")
+	public void deleteById(@PathVariable Integer id) {
+		studentRepository.deleteById(id);
+	}
+	
+	@GetMapping(path="/{id}")
+	public Student findById(@PathVariable Integer id) {
+		return studentRepository.findById(id).get();
 	}
 }
